703c73cafa64ca7924ef8629d6bc01e15f390ad3
nHitzinger <nhitzinger@student.tgm.ac.at>
2022-12-14 15:40:48 +0100

    Test Data Sql without JSON

---
+++ TestData/testDataWithoutJSON.sql
@@ -0,0 +1,129 @@
+DROP TABLE IF EXISTS `myTable`;
+
+CREATE TABLE `myTable` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `numberrange` mediumint default NULL,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
+
+INSERT INTO `myTable` (`name`,`numberrange`)
+VALUES
+  ("Kameko Robertson",9),
+  ("Galena Spencer",4),
+  ("Brandon Valencia",5),
+  ("Sydnee Quinn",8),
+  ("Keane Moss",9),
+  ("Dara Mccullough",6),
+  ("Adele Galloway",8),
+  ("Sonia Faulkner",3),
+  ("Audra Scott",6),
+  ("Ariana Guzman",7);
+INSERT INTO `myTable` (`name`,`numberrange`)
+VALUES
+  ("Lewis Pace",6),
+  ("Gretchen Baldwin",1),
+  ("Paul Quinn",7),
+  ("Flavia Holland",7),
+  ("Mariko Snow",10),
+  ("Whilemina Holcomb",3),
+  ("Ruby Bauer",2),
+  ("Kareem Emerson",3),
+  ("Ryan Burton",2),
+  ("Jade Cooley",5);
+INSERT INTO `myTable` (`name`,`numberrange`)
+VALUES
+  ("Stella Colon",8),
+  ("Teegan Bauer",9),
+  ("Francesca Weaver",5),
+  ("Kristen Craig",8),
+  ("Brett Slater",3),
+  ("Aladdin Cook",7),
+  ("Jasper Contreras",9),
+  ("Cedric Daniel",5),
+  ("Martin Richmond",5),
+  ("Macey Chaney",1);
+INSERT INTO `myTable` (`name`,`numberrange`)
+VALUES
+  ("Bruce Vargas",9),
+  ("Louis Ayala",1),
+  ("Rebekah Jenkins",3),
+  ("Seth Burks",3),
+  ("Arthur Dotson",0),
+  ("Carolyn Flores",4),
+  ("Lois Singleton",9),
+  ("Carol Shepard",8),
+  ("Larissa Vaughn",6),
+  ("Oleg Shaw",4);
+INSERT INTO `myTable` (`name`,`numberrange`)
+VALUES
+  ("Melodie Oliver",7),
+  ("Odette Harvey",4),
+  ("Nina Dawson",1),
+  ("Melanie Gomez",5),
+  ("Christine Boyle",2),
+  ("Christopher Barry",1),
+  ("Malik Holloway",5),
+  ("Vivien Mills",1),
+  ("August Duke",2),
+  ("Addison Cook",6);
+INSERT INTO `myTable` (`name`,`numberrange`)
+VALUES
+  ("Karyn Dickerson",6),
+  ("Ronan Brock",5),
+  ("Elmo Fernandez",10),
+  ("Rebekah Bryant",2),
+  ("Len Rutledge",3),
+  ("Amelia Garrison",2),
+  ("Ulla Weeks",5),
+  ("Wing Moreno",2),
+  ("MacKenzie Holloway",7),
+  ("Keelie Duncan",3);
+INSERT INTO `myTable` (`name`,`numberrange`)
+VALUES
+  ("Astra Sweet",6),
+  ("Jonah Pearson",5),
+  ("Laurel Delacruz",3),
+  ("Anjolie Hull",7),
+  ("Christopher Clements",1),
+  ("Kasper Harrington",0),
+  ("Amber Wiley",6),
+  ("Pearl Petersen",5),
+  ("Chiquita Deleon",6),
+  ("Jarrod Castro",3);
+INSERT INTO `myTable` (`name`,`numberrange`)
+VALUES
+  ("Katell Mooney",2),
+  ("Sarah Mitchell",9),
+  ("Burke Barr",10),
+  ("Dean Clarke",4),
+  ("Davis Gilbert",3),
+  ("Velma Thornton",0),
+  ("Fitzgerald Roach",10),
+  ("Aladdin Mcguire",4),
+  ("Heidi Joyner",1),
+  ("Brock Guthrie",4);
+INSERT INTO `myTable` (`name`,`numberrange`)
+VALUES
+  ("Rhonda Farley",4),
+  ("Orla Reeves",1),
+  ("Slade Washington",7),
+  ("Berk Weeks",3),
+  ("Jaime Massey",6),
+  ("Shafira Velez",1),
+  ("Duncan Hull",5),
+  ("Grant Graham",6),
+  ("Burke Rogers",2),
+  ("Bethany Calhoun",1);
+INSERT INTO `myTable` (`name`,`numberrange`)
+VALUES
+  ("Jacob Weaver",1),
+  ("Kasper Willis",7),
+  ("Joseph Morin",7),
+  ("Mark Estes",7),
+  ("McKenzie Meadows",3),
+  ("Scarlet Norris",4),
+  ("Halee Hebert",2),
+  ("Anjolie Mckay",1),
+  ("Sybil Moreno",1),
+  ("Dylan Benjamin",2);
